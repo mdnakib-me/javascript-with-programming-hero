e8ed2280bea2645bcbf2cda5d2c9f94a98496534
mdnakib-me <mdnakib.me@gmail.com>
2026-08-24 05:16:01 +0600

    4 th lesson

--- module-1/lesson (3)/script.js
+++ module-1/lesson (3)/script.js
@@ -18,14 +18,51 @@ console.log(sub)
 // that's a power
 const power = a**b;
 console.log(power)
-// 
-// const pone = 1++;
-console.log(pone)
+//that's decrement operators
+let j = 7;
+j--;
+console.log(j)
+// that's increment operators
+let jg =3;
+jg++;
+console.log(jg)
 // assignment operators
+// + velue assign
+let num =10;
+num += 5;
+console.log("+assign", num)
+ // - value assign
+ num -= 5;
+ console.log("- assign", num)
+ // * value assign
+ num *= 2;
+ console.log("*", num)
+ // / value assign
+ num /=2;
+ console.log("/", num)
+// % valu assign
+num %= 8;
+console.log("%", num)
 // comparison operators
-const c = a>b;
+const c = a>b; 
 console.log(c)
 const d = a<b;
 console.log(d)
 const e = a>=b;
 console.log(e)
+let p = 10;
+let q = 5;
+console.log(p == q);    // Equal => false
+console.log(p != q);    // Not Equal => true
+
+console.log(p === q);   // Strict Equal => false
+console.log(p !== q);   // Strict Not Equal => true
+
+console.log(p > q);     // Greater than => true
+console.log(p < q);     // Less than => false
+
+console.log(p >= q);    // Greater than or equal => true
+console.log(p <= q);    // Less than or equal => false
+
+console.log(10 == "10");
+console.log(100 === "20");     
--- module-1/lesson (4)/script.js
+++ module-1/lesson (4)/script.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+let firstName = "Nakib"
+let lastName = "Isalm"
+// let fullName = firstName + lastName;
+// let fullName = firstName +" "+ lastName;
+//let fullName = `${firstName} + ${lastName}`
+let fullName = `${firstName} ${lastName} \nKishoreganj`
+//console.log(fullName)
+let ramstu = 'I am a "student" ';
+console.log(ramstu)
+let address = "Kishoreganj";
+console.log(address.toUpperCase());
+console.log(ramstu.includes("student"));
+console.log(ramstu.length);
