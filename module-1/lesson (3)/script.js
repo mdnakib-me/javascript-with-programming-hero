ecc4ffa02746381044930689622aea9d5b35529b
mdnakib-me <mdnakib.me@gmail.com>
2026-08-24 03:52:26 +0600

    lessen3

--- module-1/lesson (3)/script.js
+++ module-1/lesson (3)/script.js
@@ -1,10 +1,31 @@
 const a = 10;
 const b = 3;
+// that's summational operators
 const sum = a+b;
 console.log(sum)
-
+// that's divitational operators
 const div = a / b;
 console.log(div)
-
+// that's is a modulus operators
 const rmdr = a % b;
 console.log("reminded :-", rmdr)
+// that's is a mul 
+const mol = a*b;
+console.log(mol)
+// this is a sub
+const sub = a-b;
+console.log(sub)
+// that's a power
+const power = a**b;
+console.log(power)
+// 
+// const pone = 1++;
+console.log(pone)
+// assignment operators
+// comparison operators
+const c = a>b;
+console.log(c)
+const d = a<b;
+console.log(d)
+const e = a>=b;
+console.log(e)
