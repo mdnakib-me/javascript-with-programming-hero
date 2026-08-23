485b89418430385f5267f8c11847c6aef9c1b4fe
mdnakib-me <mdnakib.me@gmail.com>
2026-08-23 06:41:27 +0600

    1 st done

--- module-1/lesson/script.js
+++ module-1/lesson/script.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+// আপনার কাঙ্খিত ডাটা বা মেসেজ
+let message = "হ্যালো! এই লেখাটি এখন HTML পেইজে দেখা যাচ্ছে।";
+
+// console.log এ দেখার জন্য
+console.log(message);
+
+// HTML পেইজে দেখানোর জন্য নিচের লাইনটি লিখুন
+document.getElementById("output").innerHTML = message;
+
+const a = 5;
+console.log(a)
+document.getElementById("a").innerHTML = a;
