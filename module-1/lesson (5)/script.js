bde290396bd0b07a917e4f42072d10b3cf815c72
mdnakib-me <mdnakib.me@gmail.com>
2026-08-24 16:09:10 +0600

    5th lesson done

--- module-1/lesson (5)/script.js
+++ module-1/lesson (5)/script.js
@@ -1,1 +1,54 @@
-console.log("hello world")
+let age = 2;
+if (age > 18) {
+    console.log("Voter")
+} else {
+    console.log("Not a Voter")
+}
+
+
+//  ///
+//  let marks = 25;
+//  if (marks >= 80) {
+//      console.log("A+");
+//  } else if (marks >= 70) {
+//      console.log("A");
+//  } else if (marks >= 60) {
+//      console.log("A-");
+//  } else if (marks >= 50) {
+//      console.log("B");
+//  } else if (marks >= 40) {
+//      console.log("C");
+//  } else if (marks >= 33) {
+//      console.log("D");
+//  } else {
+//      console.log("F")
+//  }
+
+// //
+let marks = 32;
+switch (true) {
+    case marks >= 80:
+        console.log("A+");
+        break;
+    case marks >= 70:
+        console.log("A-");
+        break;
+    case marks >= 60:
+        console.log("A");
+        break;
+    case marks >= 50:
+        console.log("B");
+        break;
+    case marks >= 40:
+        console.log("C");
+        break;
+    case marks >= 33:
+        console.log("D");
+        break;
+    default:
+        console.log("Saleee tui fail korchis")
+        break;
+}
+ 
+
+age!=18 ? console.log("Voter") : console.log("Not Voter");
