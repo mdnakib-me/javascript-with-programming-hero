5edc93dfaf1aa9914c99fa6e6fceeedcd79ef7d9
Md. Tarikul Islam Bhuyian Nakib <mdnakib.me@gmail.com>
2026-09-25 20:11:48 +0600

    Add username validation function

--- Foundation-Program-Assignment-1/answer.js
+++ Foundation-Program-Assignment-1/answer.js
@@ -27,3 +27,15 @@ function getDayType(day) {
       return "Invalid Day";
   }
 }
+//question3 answer 
+function validateUsername(username) {
+  if (username.length < 4) {
+      return "Too Short";
+  }else if (username.includes(" ")) {
+      return "No Space Allowed";
+  }else if (username.toLowerCase().includes("admin")) {
+      return "Reserved Word";
+  }else {
+      return "Available"
+  }
+}
