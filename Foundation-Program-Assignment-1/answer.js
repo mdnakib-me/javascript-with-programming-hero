7862ee837ffffeca24a873f442d5abf93f767562
Md. Tarikul Islam Bhuyian Nakib <mdnakib.me@gmail.com>
2026-09-25 08:44:07 +0600

    Implement getDayType function for day classification
    Added a function to determine the type of day (Weekend or Working Day).

--- Foundation-Program-Assignment-1/answer.js
+++ Foundation-Program-Assignment-1/answer.js
@@ -8,3 +8,22 @@ function describeValue(value) {
     return `${type} | falsy`;
   }
 }
+//question2 with answer 
+function getDayType(day) {
+  const dayName = day.toLowerCase();
+  switch (dayName) {
+    case "friday":
+    case "saturday":
+      return "Weekend";
+
+    case "sunday":
+    case "monday":
+    case "tuesday":
+    case "wednesday":
+    case "thursday":
+      return "Working Day";
+
+    default:
+      return "Invalid Day";
+  }
+}
