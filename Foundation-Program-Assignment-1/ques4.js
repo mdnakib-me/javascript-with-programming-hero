02bf7592ba9ff55599e4f625bc1b51c4c894b285
Md. Tarikul Islam Bhuyian Nakib <mdnakib.me@gmail.com>
2026-09-25 20:33:13 +0600

    Add getCngFare function for fare calculation
    Implement a function to calculate CNG fare based on distance, night charges, and waiting time.

---
+++ Foundation-Program-Assignment-1/ques4.js
@@ -0,0 +1,18 @@
+function getCngFare(distance, isNight = false, waitingMinutes=0){
+    let fare = 50;
+    if (distance > 2){
+      fare += (distance - 2) * 15;
+    }
+      fare += waitingMinutes*2;
+    if (isNight) {
+      fare *= 1.20;
+    }
+    return fare;
+}
+console.log(getCngFare(1));
+console.log(getCngFare(2));
+console.log(getCngFare(5));
+console.log(getCngFare(10));
+console.log(getCngFare(5,false,10));
+console.log(getCngFare(5,true));
+console.log(getCngFare(5,true,10));
