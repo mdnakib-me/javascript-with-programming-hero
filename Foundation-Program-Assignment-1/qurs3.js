b1d6bf3f88b3aa273bc7fa55a254284d760b85b7
Md. Tarikul Islam Bhuyian Nakib <mdnakib.me@gmail.com>
2026-09-25 20:09:27 +0600

    Add username validation function
    This function checks if a username meets specific criteria, such as length, spaces, and reserved words.

---
+++ Foundation-Program-Assignment-1/qurs3.js
@@ -0,0 +1,20 @@
+function validateUsername(username) {
+  if (username.length < 4) {
+      return "Too Short";
+  }else if (username.includes(" ")) {
+      return "No Space Allowed";
+  }else if (username.toLowerCase().includes("admin")) {
+      return "Reserved Word";
+  }else {
+      return "Available"
+  }
+}
+console.log(validateUsername("admin"));
+console.log(validateUsername("rahim123"));
+console.log(validateUsername("ab"));
+console.log(validateUsername("a b"));
+console.log(validateUsername("abcd"));
+console.log(validateUsername("rahim islam"));
+console.log(validateUsername("superadmin99"));
+console.log(validateUsername("Admin_rahim"));
+console.log(validateUsername("Nakib"));
