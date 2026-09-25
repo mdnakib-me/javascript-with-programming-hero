848a5a2f5ceca4ddcdccf131071a8bed70e70f16
Md. Tarikul Islam Bhuyian Nakib <mdnakib.me@gmail.com>
2026-09-25 20:34:11 +0600

    Implement CNG fare calculation function
    Add function to calculate CNG fare based on distance, night time, and waiting time.

--- Foundation-Program-Assignment-1/answer.js
+++ Foundation-Program-Assignment-1/answer.js
@@ -39,3 +39,15 @@ function validateUsername(username) {
       return "Available"
   }
 }
+//question4 answer 
+function getCngFare(distance, isNight = false, waitingMinutes=0){
+    let fare = 50;
+    if (distance > 2){
+      fare += (distance - 2) * 15;
+    }
+      fare += waitingMinutes*2;
+    if (isNight) {
+      fare *= 1.20;
+    }
+    return fare;
+}
