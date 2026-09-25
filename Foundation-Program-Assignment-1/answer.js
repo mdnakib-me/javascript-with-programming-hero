40aaa5f4207e7d988a270d42cf32603a61293e23
Md. Tarikul Islam Bhuyian Nakib <mdnakib.me@gmail.com>
2026-09-25 21:46:08 +0600

    Add getChaseVerdict function for cricket scoring

--- Foundation-Program-Assignment-1/answer.js
+++ Foundation-Program-Assignment-1/answer.js
@@ -51,3 +51,29 @@ function getCngFare(distance, isNight = false, waitingMinutes=0){
     }
     return fare;
 }
+//question5 answer 
+const getChaseVerdict = (target, scored, ballsLeft) => {
+  const runsNeeded = target - scored;
+
+  if (runsNeeded <= 0) {
+    return "Won";
+  }
+
+  if (ballsLeft <= 0) {
+    return "Lost";
+  }
+
+  const requiredRate = (runsNeeded / ballsLeft) * 6;
+
+  let verdict;
+
+  if (requiredRate <= 6) {
+    verdict = "Comfortable";
+  } else if (requiredRate <= 12) {
+    verdict = "Tough";
+  } else {
+    verdict = "Almost Impossible";
+  }
+
+  return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
+};
